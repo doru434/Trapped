1a868e8af6db7b2e31a8f2ac5762e123a1e92366
PanSkrzynka <panskrzynka.dev@gmail.com>
2019-01-11 08:42:17 +0100

    Enemy Basic States + Goblin placeholder model, potrzebowalem czegoś z animacjami, wiem ze troche wazy :/

---
+++ Trapped/Assets/Scripts/EnemyScripts/ChasingState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasingState : EnemyState
+{
+    public ChasingState(Enemy enemy) : base(enemy)
+    {
+        Debug.Log("chesingState");
+    }
+
+    public override void StateTick()
+    {
+        //DO zmiany
+        enemy.NavMeshAgent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
+    }
+
+    public override EnemyState UpdateState()
+    {
+        //Logic to IdleState
+        if (enemy.Disttoplayer() > 11.0f)
+            return new IdleState(enemy);
+
+        return this;
+    }
+}
---
+++ Trapped/Assets/Scripts/EnemyScripts/IdleState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : EnemyState
+{
+    private float idleTime;
+
+    public IdleState(Enemy enemy) : base(enemy)
+    {
+        idleTime = 5.0f;
+        Debug.Log("IdleState");
+    }
+
+    public override void StateTick()
+    {
+        idleTime -= Time.deltaTime;
+    }
+
+    public override EnemyState UpdateState()
+    {
+
+        //Logic to ChasingState
+        if(enemy.Disttoplayer() < 10.0f)
+        return new ChasingState(enemy);
+        //Logic to FightState
+        if(enemy.Disttoplayer() < 1.0f)
+        return new FightState(enemy);
+        //Logic to PatrolState
+        if(idleTime <= 0.0f)
+        return new PatrolState(enemy);
+        
+    
+        return this;
+    }
+}
+
